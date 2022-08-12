b05ab0b9165a97e7552f6266d1b8bb024cbfba43
ingring <ingrid.gladheim@student.uib.no>
2022-08-12 21:00:38 +0200

    navbar on desktop, om oss, bedrifter

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,103 +1,46 @@
-// if (window.innerWidth < 960) {
-//     const dropdown = document.querySelector("#dropdown");
+// changing navbar according to screen size (media queries) 
+const dropdown = document.querySelector("#dropdown");
+const dropdownBtn = document.querySelector("#dropdown__btn");
+const dropdownBtnClose = document.querySelector("#dropdown__btn--close");
+const dropdownContent = document.querySelector("#dropdown__content");
 
-//     dropdown.style.visibility = "hidden";
+const mediaQuery = window.matchMedia('(min-width: 500px)');
 
-//     document.querySelector("#dropdown__btn").onclick = function() {
-//         dropdown.style.visibility = "visible";
-//     }
-
-//     document.querySelector("#dropdown__btn--close").onclick = function() {
-//         dropdown.style.visibility = "hidden";
-//     }
-// }
-
-// else {
-//     dropdown.style.visibility = "visible";
-
-//     document.querySelector("#dropdown__btn").style.visibility = "hidden";
-
-//     document.querySelector("#dropdown__btn--close").style.visibility = "hidden";
-
-// }
-
-
-
-//hidden them
-// dropdown.style.visibility = "visible";
-
-// document.querySelector("#dropdown__btn").style.visibility = "hidden";
-
-// document.querySelector("#dropdown__btn--close").style.visibility = "hidden";
-
-
-
-//det første som funka
-// const dropdown = document.querySelector("#dropdown");
-
-// dropdown.style.visibility = "hidden";
-
-// document.querySelector("#dropdown__btn").onclick = function() {
-//     dropdown.style.visibility = "visible";
-// }
-
-// document.querySelector("#dropdown__btn--close").onclick = function() {
-//     dropdown.style.visibility = "hidden";
-// }
-
-
-
-
-const mediaQuery = window.matchMedia('(min-width: 768px)')
 function handleTabletChange(e) {
   // Check if the media query is true
   if (e.matches) {
     dropdown.style.visibility = "visible";
+    dropdownBtn.style.display = "none";
+    dropdownBtnClose.style.display = "none";
 
-    // document.querySelector("#dropdown__btn").style.visibility = "hidden";
-    document.querySelector("#dropdown__btn").style.display = "none";
-
-    // document.querySelector("#dropdown__btn--close").style.visibility = "hidden";
-    document.querySelector("#dropdown__btn--close").style.display = "none";
-
-    document.querySelector("#dropdown__content").classList.remove("dropdown__content");
-
-    document.querySelector("#dropdown__content").classList.add("dropdown__content--desktop");
+    dropdownContent.classList.remove("dropdown__content");
+    dropdownContent.classList.add("dropdown__content--desktop");
 
     }
 
   else {
-        // Then log the following message to the console
-        const dropdown = document.querySelector("#dropdown");
-
         dropdown.style.visibility = "hidden";
 
-        document.querySelector("#dropdown__content").classList.add("dropdown__content");
+        dropdownContent.classList.add("dropdown__content");
 
-        document.querySelector("#dropdown__btn").style.display = "flex";
-        document.querySelector("#dropdown__btn--close").style.display = "flex";
+        dropdownBtn.style.display = "flex";
+        dropdownBtnClose.style.display = "flex";
 
-        document.querySelector("#dropdown__btn").onclick = function() {
+        dropdownBtn.onclick = function() {
             dropdown.style.visibility = "visible";
         }
     
-        document.querySelector("#dropdown__btn--close").onclick = function() {
+        dropdownBtnClose.onclick = function() {
             dropdown.style.visibility = "hidden";
         }
     }
 }
-
 
 // Register event listener
 mediaQuery.addListener(handleTabletChange)
 
 // Initial check
 handleTabletChange(mediaQuery)
-
-
-document.querySelector("#dropdown__btn--close").onclick = function() {
-    dropdown.style.visibility = "hidden";
-}
 
 
 //Function that scroll back to top when 'footerBtn' is clicked
